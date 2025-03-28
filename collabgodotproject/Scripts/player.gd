0c7d1d5d87c2985f53b137f642d6b6b532de47e5
Ray-Blast <brauliopimentel3@gmail.com>
2025-03-28 02:02:05 -0500

    added the base class for pickups, gonna figure out how godot does inheritance later on and build the framework off of that

--- collabgodotproject/Scripts/player.gd
+++ collabgodotproject/Scripts/player.gd
@@ -3,16 +3,16 @@ extends CharacterBody2D
 
 const BASE_SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@onready var char_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var direction = Vector2()
 
+@onready var char_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
